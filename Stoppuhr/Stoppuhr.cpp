c5abf505706402127cbd388e7e0533a6397aca68
nissene99 <enissenacc@protonmail.com>
2022-09-20 17:55:47 +0200

    update

---
+++ Stoppuhr/Stoppuhr.cpp
@@ -0,0 +1,31 @@
+/*
+Autor:          Erik Nissen
+Studiengang:    Informationstechnologie (INI)
+Matrikelnummer: 937388
+Datei:          Stoppuhr/Stoppuhr.cpp
+Erstellt:       20.09.2022
+*/
+
+//Includes
+#include <iostream>
+#include <chrono>
+#include <vector>
+
+//Namespaces
+namespace Stoppuhr {
+	//Class
+	class Stoppuhr {
+	private:
+		//Variables
+		std::chrono::time_point<std::chrono::high_resolution_clock> start;
+		std::chrono::time_point<std::chrono::high_resolution_clock> end;
+		std::vector<std::chrono::duration<double>> times;
+		bool running = false;
+	public:
+		//Functions
+		void startTimer();
+		void stopTimer();
+		void resetTimer();
+		void printTimes();
+	};
+}
---
+++ Stoppuhr/Stoppuhr.hpp
@@ -0,0 +1,30 @@
+/*
+Autor:          Erik Nissen
+Studiengang:    Informationstechnologie (INI)
+Matrikelnummer: 937388
+Datei:          /Stoppuhr.hpp
+Erstellt:       20.09.2022
+*/
+
+#ifndef AUFGABEN_STOPPUHR_HPP
+#define AUFGABEN_STOPPUHR_HPP
+
+namespace Stoppuhr {
+	//Class
+	class Stoppuhr {
+	private:
+		//Variables
+		std::chrono::time_point<std::chrono::high_resolution_clock> start;
+		std::chrono::time_point<std::chrono::high_resolution_clock> end;
+		std::vector<std::chrono::duration<double>> times;
+		bool running = false;
+	public:
+		//Functions
+		void startTimer();
+		void stopTimer();
+		void resetTimer();
+		void printTimes();
+	};
+}
+
+#endif //AUFGABEN_STOPPUHR_HPP
