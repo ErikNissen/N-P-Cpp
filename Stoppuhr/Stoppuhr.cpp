46c033556b998c4ede7eefd9258911681eb36851
ErikNissen <73883328+ErikNissen@users.noreply.github.com>
2022-09-20 18:14:30 +0200

    Update Stoppuhr.cpp

--- Stoppuhr/Stoppuhr.cpp
+++ Stoppuhr/Stoppuhr.cpp
@@ -23,30 +23,16 @@ namespace Stoppuhr {
 		bool running = false;
 	public:
 		//Functions
-		void startTimer(){
-			if(!running){
-				start = std::chrono::high_resolution_clock::now();
-				running = true;
-			}
-		};
-		auto diff(auto start, auto end){
-			return end - start;
-		};
-		void stopTimer(){
-			if(running){
-				end = std::chrono::high_resolution_clock::now();
-				times.push_back(diff(start, end));
-				running = false;
-			}
-		};
-		void resetTimer(){
-			times.clear();
-		};
-		void printTimes(){
-			for(auto time : times){
-				std::cout << time.count() << std::endl;
-			}
-		};
+		void startTimer(){};
+
+		void setTimer(){};
+
+		void resetTimer(){};
+
+		void printTimes(){};
+
+		long long diff(std::chrono::time_point<std::chrono
+		::high_resolution_clock> start, std::chrono::time_point<std::chrono::high_resolution_clock> end){};
 
 	};
 }
