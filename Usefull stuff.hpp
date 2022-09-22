f122e487959c094884d0ef4901b2f00cbd09f301
ErikNissen <73883328+ErikNissen@users.noreply.github.com>
2022-09-22 19:15:32 +0200

    Create Usefull stuff.hpp

---
+++ Usefull stuff.hpp
@@ -0,0 +1,18 @@
+/*
+Autor:          Erik Nissen
+Studiengang:    Informationstechnologie (INI)
+Matrikelnummer: 937388
+Datei:          /Usefull stuff.hpp
+Erstellt:       22.09.2022
+*/
+
+#ifndef AUFGABEN_USEFULL_STUFF_HPP
+#define AUFGABEN_USEFULL_STUFF_HPP
+const unsigned char AE = static_cast<unsigned char>(142);
+const unsigned char ae = static_cast<unsigned char>(132);
+const unsigned char OE = static_cast<unsigned char>(153);
+const unsigned char oe = static_cast<unsigned char>(148);
+const unsigned char UE = static_cast<unsigned char>(154);
+const unsigned char ue = static_cast<unsigned char>(129);
+const unsigned char ss = static_cast<unsigned char>(225);
+#endif //AUFGABEN_USEFULL_STUFF_HPP
