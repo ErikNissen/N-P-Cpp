a5580944275381acb1155a504a63a1d9684226b2
ErikNissen <73883328+ErikNissen@users.noreply.github.com>
2022-09-22 19:16:01 +0200

    Update Stoppuhr.cpp

--- Stoppuhr/Stoppuhr.cpp
+++ Stoppuhr/Stoppuhr.cpp
@@ -7,28 +7,60 @@ Erstellt:       22.09.2022
 */
 #include <iostream>
 #include "Stoppuhr.hpp"
+#include "..\Usefull stuff.hpp"
 
 namespace Stoppuhr{
 	void Stoppuhr::startTimer(){
-		std::cout << "Timer started" << std::endl;
+		std::cout << "Stoppuhr gestartet." << std::endl;
 		start = std::chrono::system_clock::now();
 		last = start;
 	}
-	void Stoppuhr::setTimer(){
-		std::cout << "Timer set" << std::endl;
+	void Stoppuhr::roundTimer(){
+		std::cout << "Runde gespeichert" << std::endl;
 		current = std::chrono::system_clock::now();
 		end = current;
-		times.push_back(current - last);
+		times.insert({ times.size(), current - last});
+		last = current;
 	}
 	void Stoppuhr::printTimes() {
-		std::cout << "Printing times" << std::endl;
+		long long total[] = {0, 0, 0, 0};
+
+		std::cout << "Gebe Zeiten aus:" << std::endl;
+		// format: 00:00:00.000
 		for (auto time : times) {
-			std::cout << time.count() << std::endl;
+			total[0] += std::chrono::duration_cast<std::chrono::hours>(
+					time.second).count();
+			total[1] += std::chrono::duration_cast<std::chrono
+					::minutes>(
+					time.second).count();
+			total[2] += std::chrono::duration_cast<std::chrono
+					::seconds>(
+					time.second).count();
+			total[3] += std::chrono::duration_cast<std::chrono
+					::milliseconds>(
+					time.second).count();
+			std::cout << time.first+1 << ". " <<
+			          std::chrono::duration_cast<std::chrono::hours>(
+							  time.second).count() << ":"
+			          << std::chrono::duration_cast<
+			                  std::chrono::minutes>(time.second
+							  ).count() << ":"
+			          << std::chrono::duration_cast<
+			                  std::chrono::seconds>(time.second
+							  ).count() << "."
+			          << std::chrono::duration_cast<
+			                  std::chrono::milliseconds>(time.second
+							  ).count()
+					  << std::endl;
 		}
+		std::cout << "\nGesamt: " << total[0] << ":" << total[1] <<
+		":" << total[2] << "." << total[3] <<
+		"\n" << std::endl;
 	}
 	void Stoppuhr::resetTimer(){
-		std::cout << "Timer reset" << std::endl;
+		std::cout << "Stoppuhr zur"<<ue<<"ckgesetzt." << std::endl;
 		start = std::chrono::system_clock::now();
 		last = start;
+		times.clear();
 	}
 }
