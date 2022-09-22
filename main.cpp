c9ad09805713894d48a6d2cb60c562a139a2697f
ErikNissen <73883328+ErikNissen@users.noreply.github.com>
2022-09-22 17:12:55 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include "Stoppuhr/Stoppuhr.hpp"
+#include <chrono>
+#include <thread>
 
 int main() {
+	auto stoppuhr = Stoppuhr::Stoppuhr();
+	stoppuhr.startTimer();
+	std::cout << "Sleeping for 5 second" << std::endl;
+	std::this_thread::sleep_for(std::chrono::seconds(5));
+	stoppuhr.setTimer();
+	std::cout << "Sleeping for 3 second" << std::endl;
+	std::this_thread::sleep_for(std::chrono::seconds(3));
+	stoppuhr.setTimer();
 
+	stoppuhr.printTimes();
+	stoppuhr.resetTimer();
 }
